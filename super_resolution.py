a7ca22845ba20206a0678a93593a061ecd3bec83
“cwpeng-cn” <cw.peng@foxmail.com>
2021-01-21 14:01:59 +0800

    fix bug

--- super_resolution.py
+++ super_resolution.py
@@ -48,6 +48,6 @@ for img in tqdm(img_paths):
     # --------------------------------
     # (3) save results
     # --------------------------------
-    save_path = "../sr/" + img.replace("/", "-")
+    save_path = "../sr/" + img[2:].replace("/", "-")
     print(save_path)
     util.imsave(img_E, save_path)

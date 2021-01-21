973c5ac116fb15dbe7dc7b55579a5ec25f027c58
“cwpeng-cn” <cw.peng@foxmail.com>
2021-01-21 14:49:15 +0800

    add init_alphapose.sh

--- init_alphapose.sh
+++ init_alphapose.sh
@@ -2,11 +2,10 @@ pip install pyyaml==5.2
 pip install scipy==1.1.0
 pip install torch==1.2.0 torchvision==0.4.0
 pip install pillow==6.2.2
+pip install -U -q PyDrive
 
 python -m pip install cython
 sudo apt-get install libyaml-dev
 
-cd Alphapose
-python setup.py build develop
+cd Alphapose&&python setup.py build develop
 
-pip install -U -q PyDrive
